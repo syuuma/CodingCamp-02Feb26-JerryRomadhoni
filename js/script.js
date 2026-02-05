a3a25528889f63d5464677747f86ef57d84846ac
syuuma <jack17_manutd@yahoo.co.id>
2026-02-05 20:38:59 +0700

    test commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let todos = [];
+
+function addTodo() {
+  const todoInput = document.getElementById("todo-input");
+  const todoDate = document.getElementById("todo-date");
+
+  // validasi sederhata untuk input text dan tanggal
+  if (todoInput.value.trim() === "" || todoDate.value === "") {
+    alert("Please enter a todo and select a date.");
+  } else {
+    //create
+  }
+}
+console.log("Hello, World!");
