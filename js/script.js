8a4b64b3e29821ca13bbfd9dfa1345bc02d4515b
syuuma <jack17_manutd@yahoo.co.id>
2026-02-05 21:08:51 +0700

    update 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
+// Array untuk menyimpan todo items
 let todos = [];
 
+// Fungsi untuk menambah todo item
 function addTodo() {
   const todoInput = document.getElementById("todo-input");
   const todoDate = document.getElementById("todo-date");
 
-  // validasi sederhata untuk input text dan tanggal
+  // Validasi input
   if (todoInput.value.trim() === "" || todoDate.value === "") {
-    alert("Please enter a todo and select a date.");
+    alert("Please enter both a todo item and a due date.");
   } else {
-    //create
+    // Membuat todo object
+    const newTodo = {
+      todo: todoInput.value.trim(),
+      date: todoDate.value,
+    };
+    // menambah array todo
+    todos.push(newTodo);
+
+    // Clear input setelah menambah todo
+    todoInput.value = "";
+    todoDate.value = "";
+
+    // Render todo list
+    renderTodos();
   }
 }
-console.log("Hello, World!");
+
+/// Fungsi untuk render todo items to the DOM
+function renderTodos() {
+  const todoList = document.getElementById("todo-list");
+
+  // Clear list todo saat ini
+  todoList.innerHTML = "";
+
+  // Render todo item
+  todos.forEach((item, _) => {
+    todoList.innerHTML += `
+        <li>
+            <p class="text-2xl">${item.todo} <span class="text-sm text-gray-500">(${item.date})</span></p>
+            <hr />
+        </li>`;
+  });
+}
+
+// Fungsi untuk menghapus semua todo items
+function deleteAllTodos() {
+  todos = [];
+  renderTodos();
+}
+
+// Fungsi untuk filter todo items
+function filterTodos() {
+  /// To be implemented
+}
